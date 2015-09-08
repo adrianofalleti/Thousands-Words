ba6511cb534bcb0f33dd7c597aab124fae8e4032
Adriano Falleti <adrianofalleti@MacBook-Pro-di-Adriano.local>
2015-09-08 16:25:33 +0200

    Initial Commit

---
+++ Thousands Words/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousands Words
+//
+//  Created by Adriano Falleti on 08/09/15.
+//  Copyright (c) 2015 Adriano Falleti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Thousands Words/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Thousands Words
+//
+//  Created by Adriano Falleti on 08/09/15.
+//  Copyright (c) 2015 Adriano Falleti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
